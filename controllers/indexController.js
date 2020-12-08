73fe096f74ddc3a255acdd5f29812b055ad29c09
ngan1702 <tramngantran@gmail.com>
2020-12-08 10:12:26 +0700

    search ở home

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,8 +1,15 @@
 const bookModel = require('../models/bookModel');
+const listController = require('../controllers/listController');
 
 exports.index = async (req, res, next) => {
+    const search = req.query.search;
     // Get books from model
     const books = await bookModel.list();
+    if (search)
+    {
+        res.redirect('/listbook?search='+search);
+    }
+    else
     // Pass data to view to display list of books
-    res.render('index', {title: "Trang chủ", books});
+        res.render('index', {title: "Trang chủ", books});
 };
