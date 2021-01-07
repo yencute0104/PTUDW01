4b7f262c363d271419bc392cf84cef799d1977f7
ngan1702 <tramngantran@gmail.com>
2021-01-07 20:52:36 +0700

    chỉnh màu

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -7,18 +7,17 @@ exports.add_comment = async(req, res, next) => {
     const bookID = req.params.id;
     const nickname = req.body.nickname ? req.body.nickname: req.user.username;
     const content = req.body.content;
+    const book = await bookModel.get(bookID);
 
     if ((await userModel.getNameUser(nickname) && !req.user) || nickname === "Quản trị viên")
     {
         
         const category =  await bookModel.listcategory();
-        const bookID = req.params.id;
-        const book = await bookModel.get(bookID);
         const bookCat = await bookModel.get_name_cat(book.catID);
         const relatedBook = await bookModel.getRelatedBooks(book.catID, bookID);
         // const comment = book.comment ? book.comment:[];
 
-        const perpage = 5;
+        const perpage = 4;
         const current = parseInt(req.query.page) || 1;
         const comment = await bookModel.listcomment(bookID, current, perpage);
         const count_comment = book.comment.length || 0;    
@@ -28,12 +27,12 @@ exports.add_comment = async(req, res, next) => {
         const hasNextPage = current < pages;  
         const hasPreviousPage = current > 1;
         var avatar;
-    for (id in comment)
-    {
-        avatar = await userModel.getProfilePicUser(comment[id].nickname);
-        if (avatar)
-            comment[id].avatar = avatar;
-    }
+        for (id in comment)
+        {
+            avatar = await userModel.getProfilePicUser(comment[id].nickname);
+            if (avatar)
+                comment[id].avatar = avatar;
+        }
         return res.render('./books/detail', 
         {   
             title: "Chi tiết",
@@ -47,6 +46,7 @@ exports.add_comment = async(req, res, next) => {
             show_active_2: "show active",
             err: "Nickname đã được sử dụng, vui lòng dùng tên khác",
             content,
+            nickname,
             current,
             nextPage,
             prevPage,
@@ -61,7 +61,6 @@ exports.add_comment = async(req, res, next) => {
     }
         
      // lấy cuốn sách từ id và thêm vào comment
-    const book = await bookModel.get(bookID);
     const comment = book.comment ? book.comment : [];
     const cmt = {nickname: nickname, content: content, avatar: ""};
     comment.push(cmt);
